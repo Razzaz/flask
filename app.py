4a8fc866b859b2331f7fcd7444a7ca3afd0710ee
Ridho Gani <ridhoadhadigani@gmail.com>
2021-06-18 00:20:15 +0700

    v1.1

--- app.py
+++ app.py
@@ -10,48 +10,5 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 def home():
     return render_template("index.html")
 
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        session.permanent = True
-        user = request.form["nm"]
-        session["user"] = user
-        return redirect(url_for("user"))
-    else:
-        if "user" in session:
-            return redirect(url_for("user"))
-        return render_template("login.html")
-
-
-@app.route("/user")
-def user():
-    if "user" in session:
-        user = session["user"]
-        return f"<h1>{user}</h1>"
-    else:
-        return redirect(url_for("login"))
-
-
-@app.route("/logout")
-def logout():
-    session.pop("user", None)
-    return redirect(url_for("login"))
-
-# @app.route("/test")
-# def test():
-#     return render_template("new.html")
-
-
-# @app.route("/<name>")
-# def user(name):
-#     return f"hello {name}!"
-
-
-# @app.route("/admin")
-# def admin():
-#     return redirect(url_for("user", name="Admin!"))
-
-
 if __name__ == "__main__":
     app.run(debug=True)
