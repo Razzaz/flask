e13a7184522f7de15abcfd03b1d5da44187e1ffd
Ridho Gani <ridhoadhadigani@gmail.com>
2021-06-18 11:12:18 +0700

    v1.2

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
 from datetime import timedelta
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.secret_key = "hello"
-app.permanent_session_lifetime = timedelta(minutes=5)
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.secret_key = "gru-sketch"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/", methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
